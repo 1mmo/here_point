e8f7714a731660059e01620e162ac3ddfb3a9b0e
yunost <yunost@yunost.localhost>
2021-10-26 18:08:49 +0300

    Profile web-page

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
-from .views import HerePointLoginView, HerePointLogoutView
+from .views import HerePointLoginView, HerePointLogoutView, ChangeUserInfoView, RegisterDoneView, RegisterUserView
 from .views import index, profile
 
 
 app_name = 'users'
 urlpatterns = [
+    path('register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('register/', RegisterUserView.as_view (), name='register'),
     path('profile/', profile, name='profile'),
     path('login/', HerePointLoginView.as_view(), name='login'),
     path('logout/', HerePointLogoutView.as_view(), name='logout'),
+    path('profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView, CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic.base import TemplateView
+
+from .models import AdvUser
+from .forms import ChangeUserInfoForm, RegisterUserForm
+
+
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    """Controller-class change UserInfo"""
+    model = AdvUser
+    template_name = 'users/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy("users:profile")
+    success_message = 'Данные пользователя изменены'
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
+
+
+class RegisterUserView(CreateView):
+    model = AdvUser
+    template_name = 'users/register_user.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('users:register_done')
+
+
+class RegisterDoneView(TemplateView):
+    template_name = 'users/register_done.html'
 
 
 class HerePointLogoutView(LoginRequiredMixin, LogoutView):
