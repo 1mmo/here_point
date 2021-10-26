1502f26a9e8149d596d2a79887a506153bf88ae7
yunost <yunost@yunost.localhost>
2021-10-26 14:35:01 +0300

    Profile web-page

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .views import HerePointLoginView
-from .views import index
+from .views import index, profile
 
 
 app_name = 'users'
 urlpatterns = [
+    path('profile/', profile, name='profile'),
     path('login/', HerePointLoginView.as_view(), name='login'),
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-
-from django.shortcuts import render
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 
 
 class HerePointLoginView(LoginView):
+    """ Controller-class doing the login """
     template_name = 'users/login.html'
 
 def index(request):
+    """ Main page """
     return render(request, 'users/index.html')
 
+@login_required
+# login_required checking users for registration
+def profile(request):
+    """ Controller-class that display the user profile page """
+    return render(request, 'users/profile.html')
