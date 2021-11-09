5787725cd9c89f72531cbff5989bef9dea252d74
yunost <yunost@yunost.localhost>
2021-11-09 13:15:00 +0300

    Changed admin-panel

--- src/users/admin.py
+++ src/users/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 from .models import AdvUser
 
 
-admin.site.register(AdvUser)
+class AdvUserAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'is_activated', 'date_joined')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    fields = (('username', 'email'), ('first_name', 'last_name'),
+              ('send_messages', 'is_active', 'is_activated'),
+              ('is_staff', 'is_superuser'),
+              ('last_login', 'date_joined'),
+              'groups', 'user_permissions',)
+    readonly_fields = ('last_login', 'date_joined')
+
+admin.site.register(AdvUser, AdvUserAdmin)
