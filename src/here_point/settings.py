44706776cf4557aedbe67035a1e060c06fb56813
yunost <yunost@yunost.localhost>
2021-11-23 17:16:14 +0300

    Admin

--- src/here_point/settings.py
+++ src/here_point/settings.py
@@ -85,7 +85,6 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'users.AdvUser'
 
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- src/main/admin.py
+++ src/main/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Place, City, AdditionalImage
+from users.models import AdvUser
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'order')
+    lsit_filter = ('order', 'title')
+
+
+class AdditionalImageInline(admin.TabularInline):
+    model = AdditionalImage
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('city', 'category', 'title', 'description', 
+                    'address', 'author', 'created_at')
+    fields = (('city', 'category'), 'title', 'description',
+              'address', 'image', 'is_active')
+    inlines = (AdditionalImageInline,)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('title', 'order')
